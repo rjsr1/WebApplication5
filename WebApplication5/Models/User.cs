5e1efb677b99f0a38031ad4aa7db28ce8a2451b5
Rodolfo Jose de Souza Rocha <rodolfo.rocha@datametrica.com.br>
2017-10-05 08:18:37 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication5/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public bool IsValid(string _username, string _password)
+        {
+            using (var conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rodolfo.rocha\source\repos\WebApplication5\WebApplication5\App_Data\DadosTeste.mdf;Integrated Security=True"))
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[System_Users] " +
+                       @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, conn);
+                cmd.Parameters
+                   .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                   .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = HELPERS.SHA1.Encode(_password);
+                conn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+
+                }
+            }
+        }
+    }
+}
